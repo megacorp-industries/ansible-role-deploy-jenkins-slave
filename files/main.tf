55e79f46d70bdbac6c254d21124784fa350f1f37
megacorp-industries <121791047+megacorp-industries@users.noreply.github.com>
2023-01-08 10:13:55 +0000

    Add files via upload

---
+++ files/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system" 
+}
+
+resource "libvirt_pool" "jenkins-slave-pool" {
+  name = "jenkins-slave-pool"
+  type = "dir"
+  path = "/opt/jenkins-slave-pool"
+}
+resource "libvirt_volume" "jenkins-slave-volume" {
+  name   = "jenkins-slave-volume"
+  source = "/tmp/packer/artifacts/jenkins-slave/packer-jenkins-slave"
+  pool   = libvirt_pool.jenkins-slave-pool.name
+  format = "qcow2"
+}
+
+resource "libvirt_domain" "jenkins-slave-domain" {
+  name      = "jenkins-slave"
+  memory    = "4096"
+  vcpu      = 2
+  cpu {
+    mode = "host-passthrough"
+  }
+  network_interface {
+    network_name   = "default"
+    wait_for_lease = true
+  }
+  disk {
+    volume_id = libvirt_volume.jenkins-slave-volume.id
+  }
+}
+
+output "ip" {
+  value = libvirt_domain.jenkins-slave-domain.network_interface[0].addresses[0]
+}
